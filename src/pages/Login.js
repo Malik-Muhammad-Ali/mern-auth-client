3a86eb487385275a7f15b8b0398be25612786609
Malik Muhammad Ali <muhammadali556656@gmail.com>
2024-05-26 00:25:30 +0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
     const handleLogin = async (e)=>{
         e.preventDefault();
 
-        const response = await fetch('http://localhost:6001/api/login', {
+        const response = await fetch('https://deploy-mern-ali/api/login', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
